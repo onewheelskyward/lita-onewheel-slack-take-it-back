a48fc82f4a80175e4012405a3b957f9b6c1e707a
Andrew Kreps <andrew.kreps@gmail.com>
2017-03-12 11:17:30 -0700

    yeah

--- lib/lita/handlers/onewheel_slack_take_it_back.rb
+++ lib/lita/handlers/onewheel_slack_take_it_back.rb
@@ -1,24 +1,14 @@
 module Lita
   module Handlers
     class OnewheelSlackTakeItBack < Handler
-      route(/take it back/i,
+      route(/take (it|that) back/i,
             :handle_delete)
-      route(/Good morning./,
-            :handle_cosmos)
-
-      def handle_cosmos(response)
-        resp = response.reply "Some text"
-        Lita.logger.debug resp.inspect
-        redis.set('slack_last', resp)
-        redis.set('one', 'two')
-        Lita.logger.debug "Redis response: #{m}"
-      end
+      route(/take(it|that)back/i,
+            :handle_delete,
+            command: true)
 
       def handle_delete(response)
-        Lita.logger.debug "Hangling delete"
-        Lita.logger.debug redis.get('slack_last')
-        Lita.logger.debug redis.get('one')
-        # response.reply redis.hget('stib', 'last')
+        Lita.logger.debug redis.hget('lita', 'slack_last')
       end
     end
 
