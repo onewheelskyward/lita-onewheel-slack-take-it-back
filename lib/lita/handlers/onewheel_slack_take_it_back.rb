c27664d44a8e4c59516e3385c1e40616cc30ac76
Andrew Kreps <andrew.kreps@gmail.com>
2017-03-12 10:03:45 -0700

    yeah

--- lib/lita/handlers/onewheel_slack_take_it_back.rb
+++ lib/lita/handlers/onewheel_slack_take_it_back.rb
@@ -10,7 +10,7 @@ module Lita
         resp = response.reply "Some text"
         Lita.logger.debug resp.inspect
         m = redis.hset('stib', 'last', resp.to_s)
-        redis.hset('stib', 'one', 'two')
+        redis.set('one', 'two')
         Lita.logger.debug "Redis response: #{m}"
         # redis.set('slack_last', resp)
       end
@@ -18,7 +18,7 @@ module Lita
       def handle_delete(response)
         Lita.logger.debug "Hangling delete"
         Lita.logger.debug redis.hget('stib', 'last')
-        Lita.logger.debug redis.hget('stib', 'one')
+        Lita.logger.debug redis.get('one')
         # response.reply redis.hget('stib', 'last')
       end
     end
