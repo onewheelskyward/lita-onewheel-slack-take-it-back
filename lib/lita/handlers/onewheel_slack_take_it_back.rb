2db6ef95845efe69d8cd6893a851100b422d0bdc
Andrew Kreps <andrew.kreps@gmail.com>
2017-03-26 12:26:06 -0700

    API call trials

--- lib/lita/handlers/onewheel_slack_take_it_back.rb
+++ lib/lita/handlers/onewheel_slack_take_it_back.rb
@@ -19,7 +19,9 @@ module Lita
         Lita.logger.debug Lita.adapters.inspect
         Lita.logger.debug Lita.adapters[:slack].inspect
         Lita.logger.debug Lita.adapters[:slack]::API.inspect
-        Lita.adapters[:slack]::API.new.call_api('delete')
+        slack_config = {token: config.adapters.slack.token}
+        Lita.logger.debug(slack_config)
+        Lita.adapters[:slack]::API.new(slack_config).call_api('delete')
       end
     end
 
