8e5c58ce01ebdcae159f52fe2e6e5f2aff3e1d28
Andrew Kreps <andrew.kreps@gmail.com>
2017-03-26 12:12:52 -0700

    Hmm

--- lib/lita/handlers/onewheel_slack_take_it_back.rb
+++ lib/lita/handlers/onewheel_slack_take_it_back.rb
@@ -13,6 +13,9 @@ module Lita
       def handle_delete(response)
         last_message = JSON.parse(redis.get('slack_last').gsub(/=>/, ':'))
         Lita.logger.debug last_message.inspect
+        Lita.logger.debug last_message['channel']
+        Lita.logger.debug last_message['ts']
+        Lita.logger.debug last_message['message']['user']
       end
     end
 
